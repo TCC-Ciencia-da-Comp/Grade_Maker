e860843a5c1b2aef483c35296ecf2cf187876f5d
Anderson <ab13022004@gmail.com>
2025-01-04 19:18:15 -0300

    Modificações no GlobalExceptionHandler

--- src/main/java/com/datamonki/ApiCadastro/infra/GlobalExceptionHandler.java
+++ src/main/java/com/datamonki/ApiCadastro/infra/GlobalExceptionHandler.java
@@ -8,6 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.datamonki.ApiCadastro.exceptions.IdNotFoundException;
 import com.datamonki.ApiCadastro.exceptions.ValidationException;
 import com.datamonki.ApiCadastro.response.ApiResponse;
 
@@ -17,6 +18,12 @@ public class GlobalExceptionHandler  {
     @ExceptionHandler(ValidationException.class)
     public ResponseEntity<ApiResponse> ValidationException(ValidationException e){
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).
+        body(new ApiResponse(e.getMessage(), null));
+    }
+
+    @ExceptionHandler(IdNotFoundException.class)
+    public ResponseEntity<ApiResponse> IdNotFoundException(IdNotFoundException e){
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).
         body(new ApiResponse(e.getMessage(), null));
     }
 
@@ -34,13 +41,13 @@ public class GlobalExceptionHandler  {
 
     @ExceptionHandler(DataIntegrityViolationException.class)
     public ResponseEntity<ApiResponse> DataIntegrityViolationException(DataIntegrityViolationException e){
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).
+        return ResponseEntity.status(HttpStatus.CONFLICT).
         body(new ApiResponse(e.getMessage(), null));
     }
 
     @ExceptionHandler(RuntimeException.class)
     public ResponseEntity<ApiResponse> RuntimeException(RuntimeException e){
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).
         body(new ApiResponse(e.getMessage(), null));
     }
 }
