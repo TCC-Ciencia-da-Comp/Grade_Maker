7ad33b7620d42d935d4ab10cde9082af2c09d7bf
WilkerMonky <weslleywilker10@gmail.com>
2025-01-16 17:41:19 -0300

    Controller de concecao de acesssos para usuários

---
+++ src/main/java/com/datamonki/ApiCadastro/controller/UsuarioRoleController.java
@@ -0,0 +1,36 @@
+package com.datamonki.ApiCadastro.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.datamonki.ApiCadastro.dto.UsuarioDto;
+import com.datamonki.ApiCadastro.dto.UsuarioRoleDto;
+import com.datamonki.ApiCadastro.exceptions.ValidationException;
+import com.datamonki.ApiCadastro.response.ApiResponse;
+import com.datamonki.ApiCadastro.service.UsuarioRoleService;
+import com.datamonki.ApiCadastro.service.UsuarioService;
+
+@Controller
+@RequestMapping("/public/dar-acesso")
+public class UsuarioRoleController {
+	@Autowired
+	private UsuarioRoleService usuarioRoleService;
+	
+	
+	@PostMapping
+	public ResponseEntity<ApiResponse> save(@RequestBody UsuarioRoleDto dto) {
+			try {
+				return usuarioRoleService.adicionarRoleAoUsuario(dto);
+			} catch (ValidationException e) {
+				e.printStackTrace();
+				return ResponseEntity.internalServerError().body(new ApiResponse(e.getMessage(), null));
+			} catch (Exception e) {
+				e.printStackTrace();
+				return ResponseEntity.internalServerError().body(new ApiResponse("Não foi possivel cadastrar o usuário, tente novamente, tente novamente", null));
+			}
+		}
+}
---
+++ src/main/java/com/datamonki/ApiCadastro/service/UsuarioRoleService.java
@@ -0,0 +1,57 @@
+package com.datamonki.ApiCadastro.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import com.datamonki.ApiCadastro.dto.UsuarioDto;
+import com.datamonki.ApiCadastro.dto.UsuarioRoleDto;
+import com.datamonki.ApiCadastro.exceptions.ValidationException;
+import com.datamonki.ApiCadastro.model.Role;
+import com.datamonki.ApiCadastro.model.Usuario;
+import com.datamonki.ApiCadastro.repository.RoleRepository;
+import com.datamonki.ApiCadastro.repository.UsuarioRepository;
+import com.datamonki.ApiCadastro.response.ApiResponse;
+
+import jakarta.transaction.Transactional;
+
+@Service
+public class UsuarioRoleService {
+	
+	@Autowired
+	private UsuarioRepository usuarioRepository;
+	@Autowired
+	private RoleRepository roleRepository;
+
+	private void verificar(UsuarioRoleDto dto) {
+		List<String> messages = new ArrayList<>();
+		if (dto.usuarioId() == null) {
+			messages.add("O campo  idUsuario está vazio");
+		}else if (dto.roleId() == null) {
+			messages.add("O campo  role está vazio");
+		}else if(usuarioRepository.findById(dto.usuarioId()).isEmpty()) {
+			messages.add("Usuário  não cadastrado");
+		}else if(roleRepository.findById(dto.roleId()).isEmpty()) {
+			messages.add("Role não cadastrada");
+		}	
+		
+		if (!messages.isEmpty()) {
+			throw new ValidationException(messages);
+		}
+	}
+	//Uma maneira diferente de adcionar mais um elemento em uma tabela intermediária com spring
+	@Transactional
+    public ResponseEntity<ApiResponse> adicionarRoleAoUsuario(UsuarioRoleDto dto) {
+		
+		verificar(dto);
+		Usuario usuario = usuarioRepository.findById(dto.usuarioId()).get();
+		Role role =  roleRepository.findById(dto.roleId()).get();
+		usuario.getRole().add(role);
+		return ResponseEntity.ok(new ApiResponse("Usuário cadastrado com sucesso", usuario.getUsername()));
+	}
+	
+	
+}
